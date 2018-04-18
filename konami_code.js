e6a100a8d82ce7a94b4b27595f07142f866a8ef5
Newton Migosi <newtonmigosi@gmail.com>
2018-04-18 11:47:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let series = [];
+let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
@@ -7,11 +7,11 @@ function init() {
 }
 
 function konami(e) {
-  if ( series == code) { alert("Feature Unlocked!!"); return}
-  let key = parseInt(e.detail || e.which);
-  console.log(series);
-  if( code.includes(key)) { series.push(key)} else { series = []}
-  //if ( series != code.slice(0, series.length)) {series = []}
+  const key = parseInt(e.detail || e.which);
+  if (key === code[index]) {index += 1;}
+  if (index === code.length) {
+    alert("Feature Unlocked!!!"); index =0;
+  } else { index = 0;}
   
 }
 
