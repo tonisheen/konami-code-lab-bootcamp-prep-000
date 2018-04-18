22a4dcb2323756c3980253652649eb6bb3f0c078
Newton Migosi <newtonmigosi@gmail.com>
2018-04-18 11:18:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,13 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addventListener('keydown', konami)
+}
 
+function konami(e) {
+  let series = [];
+  if (e.which === code[0]) {
+    series.push
+  }
+  series
 }
